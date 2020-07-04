64f0ac509d45a45c5c28d365a509bc52ff90a7c6
Aaron Marshall <hi@imaaronnicetomeetyou.me>
2020-07-04 23:09:16 +1000

    Moves to response-based return types (responder)

--- src/handlers/files/mod.rs
+++ src/handlers/files/mod.rs
@@ -1,35 +1,33 @@
 use crate::models::state::BucketMetadata;
+use crate::models::responses::FileResponse;
 
 use std::io::Cursor;
 
 use rocket::State;
 use rocket::Data;
 use rocket::http::ContentType;
-use rocket::response::Stream;
-use rocket::response::status::NotFound;
 
 use s3::bucket::Bucket;
 
-const CHUNK_SIZE: u64 = 1024 * 1024; // Stream files in 1MiB chunks?
-
 #[get("/api/files/<file_id>")]
-pub fn get_file(file_id: i32, bucket_metadata: State<BucketMetadata>) -> Result<Stream<Cursor<Vec<u8>>>, NotFound<String>> {
+pub fn get_file(file_id: i32, bucket_metadata: State<BucketMetadata>) -> FileResponse {
     let metadata = bucket_metadata.inner();
     let bucket_name = metadata.bucket_name;
     let region = metadata.region.clone();
     let credentials = metadata.credentials.clone();
 
-    let bucket = Bucket::new(bucket_name, region, credentials).unwrap();
     let mut cursor = Cursor::new(Vec::new());
+    let bucket = Bucket::new(bucket_name, region, credentials).unwrap();
     let result = bucket.get_object_stream_blocking(&format!("/{}", file_id), &mut cursor);
 
     match result {
         Ok(response_code) => {
-            Ok(cursor.into())
+            println!("Request finished successfully with response code {}", response_code);
+            FileResponse::ok(cursor.into())
         },
         Err(error) => {
             println!("An error occurred: {}\n\treturning NotFound", error);
-            Err(NotFound("The requested resource could not be retrieved.".into()))
+            FileResponse::not_found()
         }
     }
 }
---
+++ src/models/responses/mod.rs
@@ -0,0 +1,40 @@
+use rocket::http::{ContentType, Status};
+use rocket::response::{Responder, Stream};
+use rocket::Request;
+use std::io::Cursor;
+
+/// A response for a file stream
+pub struct FileResponse {
+    file_data: Stream<Cursor<Vec<u8>>>,
+    status: Status,
+}
+
+fn empty_cursor() -> Cursor<Vec<u8>> {
+    Cursor::new(Vec::new())
+}
+
+impl FileResponse {
+    pub fn not_found() -> FileResponse {
+        FileResponse::new(empty_cursor(), Status::NotFound)
+    }
+
+    pub fn ok(cur: Cursor<Vec<u8>>) -> FileResponse {
+        FileResponse::new(cur, Status::Ok)
+    }
+
+    pub fn new(cur: Cursor<Vec<u8>>, status: Status) -> FileResponse {
+        FileResponse {
+            file_data: cur.into(),
+            status: status,
+        }
+    }
+}
+
+impl<'r> Responder<'r> for FileResponse {
+    fn respond_to(self, req: &Request) -> rocket::response::Result<'r> {
+        rocket::response::Response::build_from(self.file_data.respond_to(req).unwrap())
+            .status(self.status)
+            .header(ContentType::JSON)
+            .ok()
+    }
+}
