2e48d4856c5c44d742cdb0ba4d30d659702319f3
Aaron Marshall <hi@imaaronnicetomeetyou.me>
2020-07-12 15:33:22 +1000

    It works!

--- src/handlers/files/mod.rs
+++ src/handlers/files/mod.rs
@@ -1,13 +1,17 @@
+use rocket::request::Outcome;
+use std::error::Error;
+use rocket::Request;
+use rocket::request::FromRequest;
 use crate::models::results::UploadResult;
 use crate::models::responses::ApiResponse;
 use crate::models::state::BucketMetadata;
 use crate::models::responses::FileResponse;
-use uuid::Uuid;
-use rocket_contrib::json::Json;
-use futures::executor::block_on;
-use futures::stream;
-use itertools::Itertools;
 use core::iter::FromIterator;
+use futures::stream;
+use rocket_contrib::json::Json;
+use rusoto_core::ByteStream;
+use tokio::runtime::Runtime;
+use uuid::Uuid;
 
 use std::io::{Cursor, Read};
 
@@ -41,10 +45,82 @@ use rusoto_s3::{PutObjectRequest, S3Client, StreamingBody, S3};
 //     }
 // }
 
-#[post("/api/files", data = "<file_data>")]
+pub trait Chunks: Sized {
+    fn chunks(self, size: usize) -> Chunk<Self> {
+        Chunk { iter: self, size }
+    }
+}
+
+impl<T: Iterator> Chunks for T {}
+
+pub struct Chunk<I> {
+    iter: I,
+    size: usize,
+}
+
+impl<I> Iterator for Chunk<I>
+where
+    I: Iterator,
+{
+    type Item = Vec<I::Item>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut chunk = Vec::new();
+
+        for _ in 0..self.size {
+            if let Some(v) = self.iter.next() {
+                chunk.push(v);
+            } else {
+                break;
+            }
+        }
+
+        if chunk.len() > 0 {
+            Some(chunk)
+        } else {
+            None
+        }
+    }
+}
+
+fn read_to_bytestream<R: Read + Send + Sync + 'static>(read: R) -> ByteStream {
+    let bytes = read.bytes(); // Iterator<Item = Result<u8>>
+    let chunks = bytes.chunks(4096); // Iterator<Item = Vec<Result<u8>>>
+    let iter_chunks = chunks.map(|b| Result::from_iter(b.into_iter())); // Iterator<Item = Result<Vec<u8>>>
+    let stream = stream::iter(iter_chunks); // Stream<Item = Result<Vec<u8>>>
+
+    ByteStream::new(stream)
+}
+
+pub struct ContentLength(i64);
+
+#[derive(Debug)]
+pub enum ContentLengthError {
+    BadCount,
+    Missing,
+    Invalid,
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for ContentLength {
+    type Error = ContentLengthError;
+
+    fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        let keys: Vec<_> = request.headers().get("Content-Length").collect();
+
+        match keys.len() {
+            0 => Outcome::Failure((Status::BadRequest, ContentLengthError::Missing)),
+            1 if keys[0].parse().unwrap_or(-1) >= 0 => Outcome::Success(ContentLength(keys[0].parse().unwrap_or(0))),
+            1 => Outcome::Failure((Status::BadRequest, ContentLengthError::Invalid)),
+            _ => Outcome::Failure((Status::BadRequest, ContentLengthError::BadCount)),
+        }
+    }
+}
+
+#[post("/api/files", format = "binary", data = "<file_data>")]
 pub fn create_file(
     file_data: Data,
-    bucket_metadata: State<BucketMetadata>
+    bucket_metadata: State<BucketMetadata>,
+    length: ContentLength,
 ) -> ApiResponse<UploadResult> {
     let file_id = Uuid::new_v4().to_simple();
     let metadata = bucket_metadata.inner();
@@ -52,31 +128,36 @@ pub fn create_file(
     let region = metadata.region.clone();
     let credentials = metadata.credentials.clone();
 
-    println!("Data length: {}", file_data.peek().iter().count());
-
-    let mut stream = file_data.open();
+    let read = file_data.open();
+    let stream = read_to_bytestream(read);
 
     let client = S3Client::new_with(
         HttpClient::new().expect("failed to create request dispatcher"),
         credentials,
-        Region::UsEast1,
-    );
-
-    let bitty_boys = stream::iter(
-        stream.bytes()
-            .map(Result::unwrap)
-            .chunks(4096)
-            .into_iter()
-            .map(|chunk| Ok(bytes::Bytes::from_iter(chunk)))
+        region,
     );
 
     let req = PutObjectRequest {
         bucket: bucket_name.to_owned(),
-        body: Some(StreamingBody::new(bitty_boys)),
+        key: file_id.to_string(),
+        body: Some(stream),
+        content_length: Some(length.0),
         ..Default::default()
     };
 
-    let result = block_on(client.put_object(req));
+    let future = client.put_object(req);
+    let mut runtime = match Runtime::new() {
+        Ok(rt) => rt,
+        Err(error) => {
+            println!("Failed to create tokio runtime: {}", error);
+            return ApiResponse {
+                json: Json(None),
+                status: Status::InternalServerError,
+            };
+        }
+    };
+
+    let result = runtime.block_on(future);
 
     match result {
         Ok(put_output) => {
